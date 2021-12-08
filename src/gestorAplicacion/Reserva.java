e43d758da7bc36ab57ca484e9978731cfbeda16e
Keder <Kmaderap@unal.edu.co>
2021-12-08 12:14:28 -0500

    cambio tipo atributo ccCliente de int a "cliente"

--- src/gestorAplicacion/Reserva.java
+++ src/gestorAplicacion/Reserva.java
@@ -3,12 +3,12 @@ package gestorAplicacion;
 public class Reserva {
 private int numreserva;
 private String fechareserva;
-private int ccCliente;
+private Cliente ccCliente;
 private boolean aplicable;
 //si la reserva aun es vigente o por el contrario ya venció
 
 
-public Reserva(int numreserva, String fechareserva, int ccCliente, boolean aplicable) {
+public Reserva(int numreserva, String fechareserva,Cliente ccCliente, boolean aplicable) {
 	super();
 	this.numreserva = numreserva;
 	this.fechareserva = fechareserva;
@@ -35,12 +35,8 @@ public void setFechareserva(String fechareserva) {
 	this.fechareserva = fechareserva;
 }
 
-public int getCcCliente() {
-	return ccCliente;
-}
-public void setCcCliente(int ccCliente) {
-	this.ccCliente = ccCliente;
-}
+
+
 public boolean isAplicable() {
 	return aplicable;
 }
@@ -49,6 +45,15 @@ public boolean isAplicable() {
 public void setAplicable(boolean aplicable) {
 	this.aplicable = aplicable;
 }
+
+public Cliente getCcCliente() {
+	return ccCliente;
+}
+
+public void setCcCliente(Cliente ccCliente) {
+	this.ccCliente = ccCliente;
+}
+
 
 
 }
