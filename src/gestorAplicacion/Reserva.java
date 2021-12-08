6e91248b16eb28bc2f9cd138fd3f1b2c79d3e2b5
Keder <Kmaderap@unal.edu.co>
2021-12-08 11:42:17 -0500

    Creacion sede y reserva ahora si de verdad cienporcienrealnofeik

--- src/gestorAplicacion/Reserva.java
+++ src/gestorAplicacion/Reserva.java
@@ -1,5 +1,55 @@
 package gestorAplicacion;
 
 public class Reserva {
+private int numreserva;
+private String fechareserva;
+private int ccCliente;
+private boolean aplicable;
+//si la reserva aun es vigente o por el contrario ya venció
+
+
+public Reserva(int numreserva, String fechareserva, int ccCliente, boolean aplicable) {
+	super();
+	this.numreserva = numreserva;
+	this.fechareserva = fechareserva;
+	this.ccCliente = ccCliente;
+	this.aplicable = aplicable;
+	}
+
+public int getNumreserva() {
+	return numreserva;
+}
+
+
+public void setNumreserva(int numreserva) {
+	this.numreserva = numreserva;
+}
+
+
+public String getFechareserva() {
+	return fechareserva;
+}
+
+
+public void setFechareserva(String fechareserva) {
+	this.fechareserva = fechareserva;
+}
+
+public int getCcCliente() {
+	return ccCliente;
+}
+public void setCcCliente(int ccCliente) {
+	this.ccCliente = ccCliente;
+}
+public boolean isAplicable() {
+	return aplicable;
+}
+
+
+public void setAplicable(boolean aplicable) {
+	this.aplicable = aplicable;
+}
+
 
 }
+
--- src/gestorAplicacion/Sede.java
+++ src/gestorAplicacion/Sede.java
@@ -1,5 +1,63 @@
 package gestorAplicacion;
 
 public class Sede {
+private int numsede;
+private boolean open;
+//para saber si la sede esta abierta en el momento
+private boolean disp;
+//disponibilidad de la sede (si hay mesas libres o no)
+private String ubicacion;
+private int Cantidademp;
+//cantidad de empleados activos en la sede
+
+public Sede(int numsede, boolean open, boolean disp, String ubicacion, int cantidademp) {
+	super();
+	this.numsede = numsede;
+	this.open = open;
+	this.disp = disp;
+	this.ubicacion = ubicacion;
+	Cantidademp = cantidademp;
+}
+
+public int getNumsede() {
+	return numsede;
+}
+
+public void setNumsede(int numsede) {
+	this.numsede = numsede;
+}
+
+public boolean isOpen() {
+	return open;
+}
+
+public void setOpen(boolean open) {
+	this.open = open;
+}
+
+public boolean isDisp() {
+	return disp;
+}
+
+public void setDisp(boolean disp) {
+	this.disp = disp;
+}
+
+public String getUbicacion() {
+	return ubicacion;
+}
+
+public void setUbicacion(String ubicacion) {
+	this.ubicacion = ubicacion;
+}
+
+public int getCantidademp() {
+	return Cantidademp;
+}
+
+public void setCantidademp(int cantidademp) {
+	Cantidademp = cantidademp;
+}
+
 
 }
