8745d9e404667ce7fec35431e8ecb168afc2c61e
Omar Chavez <ochavez@unal.edu.co>
2021-12-16 09:29:45 -0500

    Cambio ruta serializadora

--- src/baseDatos/Serializadora.java
+++ src/baseDatos/Serializadora.java
@@ -43,56 +43,56 @@ public class Serializadora implements Serializable {
 
         try {
             //Serializa la lista de clientes
-            Escritorclientes = new FileOutputStream(System.getProperty("user.dir") + "\\tmp\\Listaclientes.txt");
+            Escritorclientes = new FileOutputStream(System.getProperty("user.dir") + "\\.temp\\Listaclientes.txt");
             ObjectOutputStream out = new ObjectOutputStream(Escritorclientes);
             out.writeObject(Cliente.clientes);
             out.close();
             Escritorclientes.close();
 
             //Serializa la lista de empleados
-            Escritorempleados = new FileOutputStream(System.getProperty("user.dir") + "\\tmp\\Listaempleados.txt");
+            Escritorempleados = new FileOutputStream(System.getProperty("user.dir") + "\\.temp\\Listaempleados.txt");
             ObjectOutputStream out2 = new ObjectOutputStream(Escritorempleados);
             out.writeObject(Empleado.empleados);
             out2.close();
             Escritorclientes.close();
 
             //Serializa la lista de comidasede
-            Escritorcomidasede = new FileOutputStream(System.getProperty("user.dir") + "\\tmp\\Listacomidasede.txt");
+            Escritorcomidasede = new FileOutputStream(System.getProperty("user.dir") + "\\.temp\\Listacomidasede.txt");
             ObjectOutputStream out3 = new ObjectOutputStream(Escritorcomidasede);
             out.writeObject(Catalogo.platos);
             out3.close();
             Escritorclientes.close();
 
             //Serializa la lista de proveedor
-            Escritorproveedor = new FileOutputStream(System.getProperty("user.dir") + "\\tmp\\Listaproveedor.txt");
+            Escritorproveedor = new FileOutputStream(System.getProperty("user.dir") + "\\.temp\\Listaproveedor.txt");
             ObjectOutputStream out4 = new ObjectOutputStream(Escritorproveedor);
             out.writeObject(Proveedor.proveedores);
             out4.close();
             Escritorproveedor.close();
 
             //Serializa la lista de pedidos
-            Escritorpedidos = new FileOutputStream(System.getProperty("user.dir") + "\\tmp\\Listapedidos.txt");
+            Escritorpedidos = new FileOutputStream(System.getProperty("user.dir") + "\\.temp\\Listapedidos.txt");
             ObjectOutputStream out5 = new ObjectOutputStream(Escritorpedidos);
             out.writeObject(Pedido.pedidos);
             out5.close();
             Escritorpedidos.close();
 
             //Serializa la lista de reservas
-            Escritorreservas = new FileOutputStream(System.getProperty("user.dir") + "\\tmp\\Listareservas.txt");
+            Escritorreservas = new FileOutputStream(System.getProperty("user.dir") + "\\.temp\\Listareservas.txt");
             ObjectOutputStream out6 = new ObjectOutputStream(Escritorreservas);
             out.writeObject(Reserva.reservas);
             out6.close();
             Escritorreservas.close();
 
             //Serializa la lista de mesas
-            Escritormesas = new FileOutputStream(System.getProperty("user.dir") + "\\tmp\\Listamesas.txt");
+            Escritormesas = new FileOutputStream(System.getProperty("user.dir") + "\\.temp\\Listamesas.txt");
             ObjectOutputStream out7 = new ObjectOutputStream(Escritormesas);
             out.writeObject(Mesa.mesas);
             out7.close();
             Escritorclientes.close();
 
             //Serializa la lista de insumos
-            Escritoinsumos = new FileOutputStream(System.getProperty("user.dir") + "\\tmp\\Listainsumos.txt");
+            Escritoinsumos = new FileOutputStream(System.getProperty("user.dir") + "\\.temp\\Listainsumos.txt");
             ObjectOutputStream out8 = new ObjectOutputStream(Escritoinsumos);
             out.writeObject(materiaPrima.insumos);
             out8.close();
@@ -121,7 +121,7 @@ public class Serializadora implements Serializable {
         FileInputStream Lectormesas;
         FileInputStream Lectorinsumos;
         try {
-            Lectorclientes = new FileInputStream(System.getProperty("user.dir") + "/tmp/Listaclientes.txt");
+            Lectorclientes = new FileInputStream(System.getProperty("user.dir") + "/.temp/Listaclientes.txt");
             ObjectInputStream in = new ObjectInputStream(Lectorclientes);
             HashMap<Integer, Cliente> cargaclientes = new HashMap<>();
             Cliente.clientes = cargaclientes;
@@ -134,7 +134,7 @@ public class Serializadora implements Serializable {
                 System.out.println(test.getCedula());
             }*/
 
-            Lectorempleados = new FileInputStream(System.getProperty("user.dir") + "/tmp/Listaempleados.txt");
+            Lectorempleados = new FileInputStream(System.getProperty("user.dir") + "/.temp/Listaempleados.txt");
             ObjectInputStream in2 = new ObjectInputStream(Lectorempleados);
             LinkedList<Empleado> cargaempleados = new LinkedList<>();
             Empleado.empleados = cargaempleados;
@@ -147,7 +147,7 @@ public class Serializadora implements Serializable {
                 System.out.println(test.getCedula());
             }*/
 
-            Lectorcomidasede = new FileInputStream(System.getProperty("user.dir") + "/tmp/Listacomidasede.txt");
+            Lectorcomidasede = new FileInputStream(System.getProperty("user.dir") + "/.temp/Listacomidasede.txt");
             ObjectInputStream in3 = new ObjectInputStream(Lectorcomidasede);
             TreeMap cargaplatos = new TreeMap<>();
             Catalogo.platos = cargaplatos;
@@ -160,7 +160,7 @@ public class Serializadora implements Serializable {
                 System.out.println(test.getCedula());
             }*/
 
-            Lectorproveedor = new FileInputStream(System.getProperty("user.dir") + "/tmp/Listaproveedor.txt");
+            Lectorproveedor = new FileInputStream(System.getProperty("user.dir") + "/.temp/Listaproveedor.txt");
             ObjectInputStream in4 = new ObjectInputStream(Lectorproveedor);
             LinkedList<Proveedor> cargaproveedores = new LinkedList<>();
             Proveedor.proveedores = cargaproveedores;
@@ -173,7 +173,7 @@ public class Serializadora implements Serializable {
                 System.out.println(test.getCedula());
             }*/
 
-            Lectorpedidos = new FileInputStream(System.getProperty("user.dir") + "/tmp/Listapedidos.txt");
+            Lectorpedidos = new FileInputStream(System.getProperty("user.dir") + "/.temp/Listapedidos.txt");
             ObjectInputStream in5 = new ObjectInputStream(Lectorpedidos);
             LinkedList<Pedido> cargapedidos = new LinkedList<>();
             Pedido.pedidos = cargapedidos;
@@ -186,7 +186,7 @@ public class Serializadora implements Serializable {
                 System.out.println(test.getCedula());
             }*/
 
-            Lectorreservas = new FileInputStream(System.getProperty("user.dir") + "/tmp/Listareservas.txt");
+            Lectorreservas = new FileInputStream(System.getProperty("user.dir") + "/.temp/Listareservas.txt");
             ObjectInputStream in6 = new ObjectInputStream(Lectorreservas);
             LinkedList<Reserva> cargareservas = new LinkedList<>();
             Reserva.reservas = cargareservas;
@@ -199,7 +199,7 @@ public class Serializadora implements Serializable {
                 System.out.println(test.getCedula());
             }*/
 
-            Lectormesas = new FileInputStream(System.getProperty("user.dir") + "/tmp/Listamesas.txt");
+            Lectormesas = new FileInputStream(System.getProperty("user.dir") + "/.temp/Listamesas.txt");
             ObjectInputStream in7 = new ObjectInputStream(Lectormesas);
             HashMap cargamesas = new HashMap<>();
             Mesa.mesas = cargamesas;
@@ -212,7 +212,7 @@ public class Serializadora implements Serializable {
                 System.out.println(test.getCedula());
             }*/
 
-            Lectorinsumos = new FileInputStream(System.getProperty("user.dir") + "/tmp/Listainsumos.txt");
+            Lectorinsumos = new FileInputStream(System.getProperty("user.dir") + "/.temp/Listainsumos.txt");
             ObjectInputStream in8 = new ObjectInputStream(Lectorinsumos);
             TreeMap cargainsumos=new TreeMap<>();
             materiaPrima.insumos = cargainsumos;
